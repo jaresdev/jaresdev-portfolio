33a115691fac1387622d7555908d05e919d99eb5
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-05 14:55:15 +0100

    Change colors

--- src/components/LanguageModal.tsx
+++ src/components/LanguageModal.tsx
@@ -28,9 +28,9 @@ const LanguageModal = ( lang ) => {
       className={`${visible ? '' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-slate-500/20 flex justify-center items-center`}
     >
 			<div className="relative p-4 w-full max-w-2xl max-h-full">
-					<div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+					<div className="relative bg-white rounded-xl shadow dark:bg-cardBackground">
 							<div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-									<h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+									<h3 className="text-xl font-semibold text-gray-700 dark:text-gray-300">
 											{userLanguage === "en" ? "English Version Available" : "Versión en español disponible"}
 									</h3>
 									<button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={closeModal}>
@@ -43,18 +43,18 @@ const LanguageModal = ( lang ) => {
 									</button>
 							</div>
 							<div className="p-4 md:p-5 space-y-4">
-									<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+									<p className="text-base leading-relaxed text-gray-900/80 dark:text-gray-500">
                     { userLanguage === "en" ? "It looks like your browser language is different from this page's language. We have an English version available." : "Parece que tu navegador tiene un idioma distinto al de la web. Hay una versión en español disponible." }
 									</p>
-									<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+									<p className="text-base leading-relaxed text-gray-900/80 dark:text-gray-500">
                     { userLanguage === "en" ? "Would you like to switch to the version in your preferred language?" : "¿Te gustaría cambiar a la versión en español?" }
 									</p>
 							</div>
 							<div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-									<button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={switchPage}>
+									<button data-modal-hide="default-modal" type="button" className="text-white border-cyan-900/10 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-cyan-900/70 hover:bg-cyan-900 focus:ring-gray-600" onClick={switchPage}>
                     { userLanguage === "en" ? "Ok, take me there!" : "Ok, ¡Vamos!" }
                   </button>
-									<button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={closeModal}>
+									<button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none rounded-lg border  focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={closeModal}>
                     { userLanguage === "en" ? "No, thanks! I'll stay here." : "¡No gracias! Me quedaré aquí." }
                   </button>
 							</div>
