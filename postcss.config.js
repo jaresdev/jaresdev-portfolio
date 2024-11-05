832e247d9c7352835811ce8da15a0f15c39a32b5
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-05 10:48:23 +0100

    Add internationalization

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,11 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
 }
