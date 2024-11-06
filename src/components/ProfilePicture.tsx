73e10b244a54a5e13a1805d7700acca12b60bcf3
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-06 10:56:51 +0100

    Create ProfilePicture component

---
+++ src/components/ProfilePicture.tsx
@@ -0,0 +1,6 @@
+const ProfilePicture = () => {
+  
+  return <img src="/profile_dark.jpg" alt="Imagen de perfil" className="inset-0 object-cover w-full h-full transition-transform duration-300 ease-in-out transform hover:scale-110" />
+}
+
+export default ProfilePicture
