275db35fef2ad3227c09d0d6e41bb717a56822c8
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-06 14:37:42 +0100

    Finish get in touch section

--- src/components/LanguageModal.tsx
+++ src/components/LanguageModal.tsx
@@ -24,7 +24,7 @@ const LanguageModal = ( lang ) => {
 
   return (
     <div
-      tabindex="-1"
+      tabIndex="-1"
       inert={!visible ? '' : null}
       className={`${visible ? '' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-slate-500/20 flex justify-center items-center`}
     >
@@ -36,7 +36,7 @@ const LanguageModal = ( lang ) => {
 									</h3>
 									<button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={closeModal}>
 											<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-													<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+													<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
 											</svg>
 											<span className="sr-only">
                         { userLanguage === "en" ? "Close modal" : "Cerrar modal" }
