065491c5028c9334a3e506294b4e55779363f69b
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-08 17:21:00 +0100

    Create Technologies carousel

---
+++ src/components/Technology.jsx
@@ -0,0 +1,9 @@
+const Technology = ({ children }) => {
+  return (
+    <div className='w-full h-full flex justify-center items-stretch p-10'>
+      {children}
+    </div>
+  )
+}
+
+export default Technology
