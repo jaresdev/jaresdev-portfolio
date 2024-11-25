0395e51c304924a53906c0b174d62bc498640115
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-25 20:00:26 +0100

    Table landscape

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -5,21 +5,25 @@ const ProfilePicture = () => {
 
   return (
     <div
-      className={`relative w-full h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-110`}
+      className={`relative w-full h-[300px] lg:h-full transition-transform duration-300 
+        ease-in-out transform hover:scale-110
+      `}
     >
       <img
         src="/profile_dark.jpg"
         alt="Imagen de perfil darkmode"
-        className={`absolute inset-0 object-cover w-full h-full transition-opacity duration-500 ease-in-out ${
-          theme === "dark" ? "opacity-100" : "opacity-0"
-        }`}
+        className={`absolute inset-0 object-cover w-full h-full transition-opacity 
+          duration-500 ease-in-out ${
+            theme === "dark" ? "opacity-100" : "opacity-0"
+          }`}
       />
       <img
         src="/profile_light.jpg"
         alt="Imagen de perfil lightmode"
-        className={`absolute inset-0 object-cover w-full h-full transition-opacity duration-500 ease-in-out ${
-          theme === "dark" ? "opacity-0" : "opacity-100"
-        }`}
+        className={`absolute inset-0 object-cover w-full h-full transition-opacity 
+          duration-500 ease-in-out ${
+            theme === "dark" ? "opacity-0" : "opacity-100"
+          }`}
       />
     </div>
   );
